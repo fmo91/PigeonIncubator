69b76e9bfcb96da0c9a70a6b631f383705cec8f7
Fernando Martin Ortiz <ortizfernandomartin@gmail.com>
2020-08-23 14:45:50 -0300

    Initial Commit

---
+++ Pigeon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pigeon
+//
+//  Created by Fernando Martín Ortiz on 23/08/2020.
+//  Copyright © 2020 Fernando Martín Ortiz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
